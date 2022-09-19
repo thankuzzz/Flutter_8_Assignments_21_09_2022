06ee292ecd53592cc0f62a0eb29e03ffa013dd2c
thankuzzz <96867490+thankuzzz@users.noreply.github.com>
2022-09-19 10:49:58 +0530

    Create Q7.dart

---
+++ Assignment_M1_OOPS1_File_Count_10/Q7.dart
@@ -0,0 +1,42 @@
+import 'dart:io';
+
+class Saving_Account{
+  static double? annualinterestrate;
+  double? saving_balance,monthly_interest;
+  static double? modify_interest_rate(double? interest_rate){
+    annualinterestrate=interest_rate;
+  }
+  calculate_monthly_interest(){
+    monthly_interest=(saving_balance!*annualinterestrate!)/12;
+  }
+  display_balance(){
+    print("Monthly Interest : $monthly_interest");
+  }
+}
+Test_Saving_Account(){
+  Saving_Account saver1=Saving_Account();
+  Saving_Account saver2=Saving_Account();
+  saver1.saving_balance=2000;
+  saver2.saving_balance=3000;
+  Saving_Account.modify_interest_rate(4);
+  saver1.calculate_monthly_interest();
+  saver2.calculate_monthly_interest();
+  stdout.write("First Account  ");
+  saver1.display_balance();
+  stdout.write("Second Account  ");
+  saver2.display_balance();
+  print("................NEXT MONTH.............");
+  Saving_Account.modify_interest_rate(5);
+  saver1.saving_balance=2000;
+  saver2.saving_balance=3000;
+  saver1.calculate_monthly_interest();
+  saver2.calculate_monthly_interest();
+  stdout.write("First Account  ");
+  saver1.display_balance();
+  stdout.write("Second Account  ");
+  saver2.display_balance();
+
+}
+main(){
+  Test_Saving_Account();
+}
