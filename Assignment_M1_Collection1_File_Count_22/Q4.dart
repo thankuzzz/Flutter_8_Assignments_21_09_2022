f8cdc6b16e9c892ac5c3cfb8c6395914ca02b507
thankuzzz <96867490+thankuzzz@users.noreply.github.com>
2022-09-20 11:46:23 +0530

    Create Q4.dart

---
+++ Assignment_M1_Collection1_File_Count_22/Q4.dart
@@ -0,0 +1,11 @@
+main(){
+  List str = [];
+  str.add("blue");
+  str.add("black");
+  str.add("yellow");
+  str.add("green");
+  var element= str.elementAt(1);
+  print(element);
+  element=str.elementAt(0);
+  print(element);
+}
