0ce4a326459b425718c076515ab664ab0c0e368c
thankuzzz <96867490+thankuzzz@users.noreply.github.com>
2022-09-23 15:47:50 +0530

    Update Q9.dart

--- Assignment_M1_OOPS1_File_Count_10/Q9.dart
+++ Assignment_M1_OOPS1_File_Count_10/Q9.dart
@@ -15,7 +15,7 @@ class Truck extends Car{
       price=regular_price!-(regular_price!/100)*10;
     }
     else{
-        price=regular_price!-(regular_price!/100)*20;
+      price=regular_price!-(regular_price!/100)*20;
 
     }
     return(price);
@@ -40,7 +40,7 @@ class Sedan extends Car{
       price=regular_price!-(regular_price!/100)*10;
     return(price);
   }
-  Sedan(this.length,int? speed, double regular_price, String color) : super(speed, regular_price, color);
+  Sedan(this.length, int? speed, double regular_price, String color) : super(speed, regular_price, color);
 }
 main()
 {
@@ -62,6 +62,7 @@ main()
     Truck t = Truck(w, speed, regular_price, color!);
     print("truck price = ${t.regular_price}");
     print("truck color = ${t.color}");
+    print("truck discount price = ${t.getSalePrice()}");
   }else if((vn=="Ford")||(vn=="ford")) {
     stdout.write("Enter the year of the vehicle : ");
     var ford_year = int.parse(stdin.readLineSync()!);
@@ -70,12 +71,14 @@ main()
     Ford f = Ford(ford_year, ford_m_d, speed, regular_price, color!);
     print("ford price = ${f.regular_price}");
     print("ford color = ${f.color}");
+    print("ford discount price = ${f.getSalePrice()}");
   }else if((vn=="Sedan")||(vn=="sedan")){
     stdout.write("Enter the length of the vehicle : ");
     var length=int.parse(stdin.readLineSync()!);
     Sedan s=Sedan(length, speed, regular_price, color!);
     print("sedan price = ${s.regular_price}");
     print("sedan color = ${s.color}");
+    print("sedan discount price = ${s.getSalePrice()}");
   }else{
     print("No such vehicle found");
   }
