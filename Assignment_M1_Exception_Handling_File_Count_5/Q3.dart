1d3a4388c30de0a81f5dcc92cc4679947cbe11f2
thankuzzz <96867490+thankuzzz@users.noreply.github.com>
2022-09-25 03:22:09 -0400

    Create Q3.dart

---
+++ Assignment_M1_Exception_Handling_File_Count_5/Q3.dart
@@ -0,0 +1,23 @@
+class Exception
+{
+  Exception(String str);
+
+
+  Exception1()
+  {
+     Exception ne1 = Exception("Hello, I am your exception");
+    throw ne1;
+  }
+}
+
+main()
+{
+  Exception em1 = Exception("");
+  try
+  {
+    em1.Exception1();
+  } catch(e)
+  {
+    print(e);
+  }
+}
