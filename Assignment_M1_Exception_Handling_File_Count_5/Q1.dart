f2d7350e61bb2c563b106d2d80b22022a7ec6cd3
thankuzzz <96867490+thankuzzz@users.noreply.github.com>
2022-09-25 02:49:06 -0400

    Update Q1.dart

--- Assignment_M1_Exception_Handling_File_Count_5/Q1.dart
+++ Assignment_M1_Exception_Handling_File_Count_5/Q1.dart
@@ -3,19 +3,15 @@ class Exception{
   Exception.named(var message){
     print(message);
   }
-  Exception(){
-
-  }
 }
 void main(){
   try{
     var c2=Exception.named("main you can't insert inside the class");
-    var c1=Exception();
   }
   catch(e){
 
   }
   finally{
-    var s1=Exception();
+    print("ended");
   }
 }
